a98f37312f5b1ea246fe2ab505c70ce3d7dfe108
vlmiroshnikov <vlmiroshnikov@gmail.com>
2022-05-13 12:40:55 +0300

    refactoring, fix test (#2)
    Co-authored-by: v.miroshnikov <v.miroshnikov@tinkoff.ru>

--- saga-core/src/test/scala/io/github/vlmiroshnikov/saga/SagaSuite.scala
+++ saga-core/src/test/scala/io/github/vlmiroshnikov/saga/SagaSuite.scala
@@ -11,17 +11,17 @@ class SagaSuite extends munit.CatsEffectSuite {
   test("build saga") {
     given Stepper[IO] = Stepper.default[IO]
 
-    val step1 = IO.println("Step-1").as(1)
-    val step2 = IO.println("Step-2") *> IO.raiseError(new Exception("errr"))
+    val step1 = IO.println("Step-1")
+    val step2 = IO.println("Step-2").as(2)
 
     val saga = for
-      first  <- step1.compensate(e => IO.println("Rollback Step1") *> IO.raiseError(new Exception("rollback error")))
+      _      <- step1.compensate(e => IO.println("Rollback Step1"))
       second <- step2.compensate(e => IO.println("Rollback Step2"))
     yield second
 
     for
       r <- saga.run()
       _ <- IO.println(r)
-    yield ()
+    yield assertEquals(r, 2)
   }
 }
